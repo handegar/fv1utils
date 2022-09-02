76799f195ae4d2a17ee7777bccddf219c37ec94a
Øystein Handegard <handegar@gmail.com>
2022-09-02 12:20:06 +0200

    Update

--- upload-rom-to-arduino.sh
+++ upload-rom-to-arduino.sh
@@ -19,6 +19,9 @@ fi
 
 # Alternatives are uni, nano etc.
 ARDUINO="diecimila"
+# Alternatives are atmega328, atmega328old
+CPU="atmega168"
+PORT="/dev/ttyUSB0"
 ROMFILE=$1
 
 # Convert the bin-file to a c-header file
@@ -28,12 +31,19 @@ mv ./programs.h arduino/EEPROMWriter/programs.h
 # Compile the writer with the embedded binary file
 echo ""
 echo "== Compiling ($ARDUINO)"
-arduino-cli compile -b arduino:avr:$ARDUINO ./arduino/EEPROMWriter
+arduino-cli compile -b arduino:avr:$ARDUINO:cpu=$CPU ./arduino/EEPROMWriter
 
 # Upload the result to an Arduino
 echo ""
-echo "== Uploading to device"
-arduino-cli upload -b arduino:avr:$ARDUINO ./arduino/EEPROMWriter
+echo "== Uploading to device ($PORT)"
+arduino-cli upload -b arduino:avr:$ARDUINO:cpu=$CPU -p $PORT ./arduino/EEPROMWriter
 
 echo ""
 echo "== Done!"
+
+echo ""
+echo "Connect your Arduino to the EEPROM as described in README.md"
+echo "file. Press the RESET button on the Arduino to start programming"
+echo "the chip. The Pin13 LED on the Arduino turns off when the write"
+echo "process has finished."
+echo ""
